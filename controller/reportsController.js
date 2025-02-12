1272707510c105e44e8b0718b573fb350c386351
paulpdoa <polopdoandres@gmail.com>
2025-02-12 21:47:16 +0800

    development of cash advance availment report

--- controller/reportsController.js
+++ controller/reportsController.js
@@ -1,21 +1,22 @@
 const Customer = require('../model/Customer');
 const Account = require('../model/Account');
+const Availment = require('../model/Availment');
 
 const serverErrorMessage = 'Internal server error, please try again';
 
 module.exports.employee_onboarding_list = async (req,res) => {
 
-    const { id } = req.params;
+    const { employer_id } = req.params;
     const title = 'EWA Employee Onboarding List';
 
     try {
         // customer CIF, customer name, account, mobile number, employer account, employer name, email address
-        const employerCustomer = await Customer.findOne({ _id: id, is_employer: true })
+        const employerCustomer = await Customer.findOne({ _id: employer_id, is_employer: true })
         .populate({
             path: 'employees'
         });
         const employerAccount = await Account.findOne({ customer_id: employerCustomer._id });
-        if(!employerCustomer) throw Error(`No employer found for ID ${id}`);
+        if(!employerCustomer) throw Error(`No employer found for ID ${employer_id}`);
         if(employerCustomer.employees.length < 1) throw Error(`No employees found for customer (${employerCustomer.customer_name})`)
         
         let employees = await Promise.all(employerCustomer.employees.map(async (employee) => {
@@ -32,8 +33,16 @@ module.exports.employee_onboarding_list = async (req,res) => {
         if(employees.length < 1) {
             employees = `No onboarded employees yet for ${employerCustomer.customer_name}`
         }
+
+        const data = { 
+            title, 
+            employer_name: employerCustomer.customer_name, 
+            employer_cif: employerCustomer._id,
+            employer_account: employerAccount.account_number, 
+            employees 
+        }
       
-        res.status(200).json({ title, employer_name: employerCustomer.customer_name, employer_cif: employerCustomer._id ,employer_account: employerAccount.account_number, employees });
+        res.status(200).json(data);
     } catch(err) {
         console.log(err);
         res.status(500).json({ message: err.message || serverErrorMessage, error: err })
@@ -42,26 +51,75 @@ module.exports.employee_onboarding_list = async (req,res) => {
 
 module.exports.cash_advance_limit_issuance = async (req,res) => {
     const { employer_id } = req.params;
+    const title = 'Cash Advance Limit Issuance Report'
 
     try {
-        const employerCustomer = await Customer.findById(employer_id);
+        const employerCustomer = await Customer.findById(employer_id)
+        .populate({
+            path: 'employees'
+        });
         if(!employerCustomer) throw Error(`Missing employer customer CIF: ${employer_id}`);
 
-        const employees = employerCustomer.employees;
+        const employerAccount = await Account.findOne({ customer_id: employer_id });
+        if(!employerAccount) throw Error(`Missing account for employer ${employerCustomer.customer_name}`)
+
+        const employerBalance = employerAccount.account_balance;
+        const employerName = employerCustomer.customer_name;
+
+        let employees = await Promise.all(employerCustomer.employees.map(async (employee) => {
+            const account = await Account.findOne({ customer_id: employee });
+            return {
+                employee_cif: employee._id,
+                account_number: account.account_number,
+                customer_name: `${employee.first_name} ${employee.middle_name} ${employee.last_name}`,
+                cash_advance_limit: employee.account_balance
+            }
+        }));
+
         if(employees.length < 1) throw Error(`No employees to be displayed for employer ${employerCustomer.customer_name}`);
 
-        for(const employee of employees) {
-            const employeeCustomer = await Customer.findById(employee);
-            if(!employeeCustomer) {
-                throw Error();
-            }
 
+        const data = {
+            title, 
+            employer_name: employerName,
+            employer_balance: employerBalance,
+            employees
         }
+
+        res.status(200).json(data);
 
     } catch(err) {
         console.log(err);
         res.status(500).json({ message: err.message || serverErrorMessage, error: err })
     }
+}
+
+// Get all advances which are not paid yet
+module.exports.cash_advance_availment = async(req,res) => {
+    const { employee_acct_no } = req.params;
+
+    try {
+        let availments;
+        
+        if(employee_acct_no) {
+            const account = await Account.findOne({ account_number: employee_acct_no }).populate('customer_id');
+            availments = await Availment.find({ account_id: account._id, is_paid: false }).populate('account_id payment_id');
+        } else {
+            availments = await Availment.find().populate('account_id payment_id');
+        }
+        
+        const data = {
+            account_number: employee_acct_no,
+            availments
+        }
+
+        res.status(200).json(data);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ message: err.message || serverErrorMessage, error: err })
+    }
     
+
+   
 }
 
--- route/reportsRoute.js
+++ route/reportsRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { employee_onboarding_list } = require('../controller/reportsController');
+const { employee_onboarding_list, cash_advance_limit_issuance, cash_advance_availment } = require('../controller/reportsController');
 const route = express.Router();
 
-route.get('/employee-onboarding-list/:id', employee_onboarding_list);
+route.get('/employee-onboarding-list/:employer_id', employee_onboarding_list);
+route.get('/cash-advance-limit-issuance/:employer_id', cash_advance_limit_issuance);
+route.get('/cash-advance-availment/:employee_acct_no', cash_advance_availment);
 
 module.exports = route;
